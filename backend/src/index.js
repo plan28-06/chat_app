c94268e851525056145c55b96e533020b207090e
plan28-06 <planar2006@gmail.com>
2025-07-19 10:11:32 +0530

    Changed location of index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authRoutes from "./src/routes/authRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 const app = express();
 
 app.use("/api/auth", authRoutes);
