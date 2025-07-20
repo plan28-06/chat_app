53ce31369a37938142e1c81d38c84edb91d776b7
plan28-06 <planar2006@gmail.com>
2025-07-20 09:23:41 +0530

    Done with signup function.

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,17 +1,23 @@
-import User from "../middlewares/user.model.js";
+import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/jwt.signer.js";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
     try {
+        if (!fullName || !password | !email) {
+            return res.status(400).json({
+                message: "All fields are required",
+            });
+        }
         if (password.length < 6) {
             return res
-                .Status(400)
+                .status(400)
                 .json({ message: "Password must be atleast 6 characters" });
         }
         const user = await User.findOne({ email });
         if (user)
-            return res.Status(400).json({ message: "Email already exists" });
+            return res.status(400).json({ message: "Email already exists" });
 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
@@ -22,11 +28,22 @@ export const signup = async (req, res) => {
             password: hashedPassword,
         });
 
-        if (newUser){
-            
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
         }
-    } catch (error) {}
-    res.send("Sign Up");
+    } catch (error) {
+        console.log("Error in sign up controller ", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
 };
 
 export const login = (req, res) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,8 @@ import { connectDB } from "./lib/db.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json());    
+
 app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
