4d4bf9d36840b21de2e992137ccc3ea179635e62
plan28-06 <planar2006@gmail.com>
2025-08-03 15:18:47 +0530

    Set things up for deployment.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,8 @@ import messageRoutes from "./routes/messageRoutes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app,server } from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
+import path from "path";
 
 app.use(express.json());
 app.use(cookieParser());
@@ -17,6 +18,13 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(process.env.PORT, () => {
     console.log(`Listening to PORT ${process.env.PORT}`);
