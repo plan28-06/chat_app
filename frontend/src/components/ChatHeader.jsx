c00fdb543f4cac21074a3a5e9c9dd92340a156f4
plan28-06 <planar2006@gmail.com>
2025-08-03 17:27:01 +0530

    Removed unnecessary comments.

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -24,9 +24,9 @@ const ChatHeader = () => {
                     <div>
                         <h3 className="font-medium">{selectedUser.fullName}</h3>
                         <p className="text-sm text-base-content/70">
-                            {/* {onlineUsers.includes(selectedUser._id)
+                            {onlineUsers.includes(selectedUser._id)
                                 ? "Online"
-                                : "Offline"} */}
+                                : "Offline"}
                         </p>
                     </div>
                 </div>
