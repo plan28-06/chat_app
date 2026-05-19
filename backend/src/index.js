980d9c382a78eaf78f07d81641be21723f3cc980
plan28-06 <planar2006@gmail.com>
2026-05-19 13:42:05 +0530

    Revert " Deployment fix"
    This reverts commit 7ceae43c59a124fd988c625c5ed938798b196177.

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,16 +16,16 @@ app.use(
     cors({
         origin: "http://localhost:5173",
         credentials: true,
-    }),
+    })
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
     });
 }
 
